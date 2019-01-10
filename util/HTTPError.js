e28e00860775d377089ab1f1b2fa93442bac55ef
violet moon <violet.wmoon@gmail.com>
2019-01-10 00:36:56 -0600

    user login connects to redux

---
+++ util/HTTPError.js
@@ -0,0 +1,12 @@
+class HTTPError extends Error {
+    constructor(statusCode, message) {
+        super(message)
+	    this.statusCode = statusCode
+	}
+
+    toString() {
+        return `HTTP ${this.statusCode}: ${this.message}`
+    }
+}
+
+module.exports={HTTPError}
